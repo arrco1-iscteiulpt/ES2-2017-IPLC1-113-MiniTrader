33f6f9be0c58b1233ef1da8612a8c3b7642219c0
Tatiana Portela <tscpa@iscte.pt>
2017-05-17 10:15:54 +0100

    group number: 113

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -24,7 +24,7 @@ import mt.filter.AnalyticsFilter;
  * MicroTraderServer implementation. This class should be responsible
  * to do the business logic of stock transactions between buyers and sellers.
  * 
- * @author Group 78
+ * @author Group 113
  *
  */
 //test commit
